5b98588a2736651cca89f31c9c971051f5e2ecbe
Adrián Fernández Carrasco <122231215+Fdezcarr@users.noreply.github.com>
2024-02-06 16:07:50 +0100

    +

--- Javascript-1/Browser object Model/cookies.js
+++ Javascript-1/Browser object Model/cookies.js
@@ -1,1 +1,43 @@
 // Guardan informacion y que no se pierda cuando refrescamos la página ( parecido a variables), pre ferencias o configuraciones del usurio de una web.
+
+const crearCookie = () => {
+  //   document.cookie = 'nombre=carlos';
+  document.cookie = 'nombre=carlos; expires=1 jan 2025 01:00:00 UTC';
+};
+
+const iniciarSesion = () => {
+  const usuario = prompt('usuario');
+  document.cookie = `nombre=${usuario}; expires=1 jan 2025 01:00:00 UTC`;
+  alert('sesion iniciada');
+};
+
+// console.log(document.cookie);
+
+/*
+Forma para obtener el usuario de las cookies
+*/
+
+let usuario;
+
+const cookies = document.cookie.split(';');
+// console.log(cookies);
+//filtramos el elemento para obtener la cookie que tiene dicha propiedad
+cookies.forEach((cookie) => {
+  const propiedad = cookie.split('=')[0];
+  if (propiedad === 'nombre') {
+    usuario = cookie.split('=')[1];
+  }
+});
+
+if (usuario) {
+  console.log('Hola ' + usuario);
+} else {
+  console.log('Por favor inicia sesión');
+}
+
+//Eliminar cookie
+
+const cerrarSesion = () => {
+  document.cookie = 'nombre=; expires=1 jan 2020 01:00:00 UTC ';
+  console.log('Hasta luego!');
+};
