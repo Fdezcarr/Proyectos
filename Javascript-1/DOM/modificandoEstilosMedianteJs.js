2408f1972c1a64d7eed09f125dda693862cf844a
Adrián Fernández Carrasco <122231215+Fdezcarr@users.noreply.github.com>
2024-03-20 14:50:29 +0100

    +1

--- Javascript-1/DOM/modificandoEstilosMedianteJs.js
+++ Javascript-1/DOM/modificandoEstilosMedianteJs.js
@@ -4,4 +4,25 @@
 
 const ultimaCaja = document.querySelector('#contenedor2 .caja:last-child');
 
-console.log(ultimaCaja);
+ultimaCaja.style.background = '#000';
+ultimaCaja.style.color = '#fff';
+
+//Aumentar y disminuir el tamaño mediante estilos en linea a través de una función.
+
+const cajas = document.querySelectorAll('.caja');
+
+let tamaño = 25;
+
+const incrementarFuente = () => {
+  cajas.forEach((caja) => {
+    tamaño++;
+    caja.style.fontSize = `${tamaño}px`;
+  });
+};
+
+const disminuirFuente = () => {
+  cajas.forEach((caja) => {
+    tamaño--;
+    caja.style.fontSize = `${tamaño}px`;
+  });
+};
